c7a28fdf878b1e04a6042df726b08f16ad5db65c
Salman Al Majali <undermod007@gmail.com>
2026-05-06 14:57:27 +0700

    feat: enhance gallery endpoint to include image URLs in response

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,23 @@
 use App\Models\Gallery;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/gallery', function(Request $request) {
-    $results = Gallery::latest()->paginate(10);
+Route::get('/gallery', function (Request $request) {
+    $results = Gallery::latest()
+        ->paginate(10)
+        ->through(function ($item) {
+            return [
+                'id'    => $item->id,
+                'image' => Storage::url($item->image),
+                'title' => $item->title ?? null,
+                'year'  => $item->year ?? null,
+            ];
+        });
 
     return response()->json([
         'success' => true,
