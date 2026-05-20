e071faa78b780476e1c9c69430d8fdf20fc9cb44
Salman Al Majali <undermod007@gmail.com>
2026-05-20 11:49:37 +0700

    refactor: replace gallery route closure with GalleryController index method

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GalleryController;
 use App\Models\Gallery;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,20 +10,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/gallery', function (Request $request) {
-    $results = Gallery::latest()
-        ->paginate(10)
-        ->through(function ($item) {
-            return [
-                'id'    => $item->id,
-                'image' => Storage::url($item->image),
-                'title' => $item->title ?? null,
-                'year'  => $item->year ?? null,
-            ];
-        });
-
-    return response()->json([
-        'success' => true,
-        'results' => $results,
-    ]);
-});
+Route::get('/gallery', [GalleryController::class, 'index']);
